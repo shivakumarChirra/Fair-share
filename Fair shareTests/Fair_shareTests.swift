ecbf337ad69d41f4e70a8c83c6f80d46250038e2
shivakumarchirra <chirrashivakumar5@gmail.com>
2025-05-19 21:51:48 +0530

    Initial Commit

---
+++ Fair shareTests/Fair_shareTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fair_shareTests.swift
+//  Fair shareTests
+//
+//  Created by shivakumar chirra on 19/05/25.
+//
+
+import Testing
+@testable import Fair_share
+
+struct Fair_shareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
